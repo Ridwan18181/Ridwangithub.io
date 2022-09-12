bc66d37f91206808dc17f4c6fec6753fc246b65f
Ridwan18181 <ridwanmaulana2728@gmail.com>
2022-09-12 22:55:02 +0700

    updet

---
+++ p03 jquery/script.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+    $("td").click(function (event) {
+      alert("Sekarang kamu berada di " + $(this).html() + "!");
+    });
+    $("th").click(function (event) {
+      alert("Sekarang kamu berada di " + $(this).html() + "!");
+    });
+  });
+
+
+
+  
